95fc11dd4d6cd7e51d995a6c40e995903701d5e6
iamsridha <124337210+iamsridha@users.noreply.github.com>
2023-02-07 09:52:32 +0530

    Add files via upload
    addition of two numbers

---
+++ addition.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class addition
+{
+public static void main(String[] args)
+{
+int i,sum=0,m1=1;
+    Scanner s=new Scanner(System.in);
+	System.out.println("Enter the number");
+	i=s.nextInt();
+for(i=1;i<=10;i++)
+{
+m1=i+sum;
+}
+System.out.println(m1);
+}
+}
