3084fd4a3cd7600d9ebcbc8b4d3a91e01b4457ba
iamsridha <124337210+iamsridha@users.noreply.github.com>
2023-02-07 09:57:54 +0530

    Add files via upload

---
+++ cube1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class cube1
+{
+    public static void main(String[] args) 
+    {
+	Scanner s = new Scanner(System.in);
+	System.out.print("Enter number:");        
+	int n=s.nextInt();
+	int i;
+	int sum=0;
+	for(i=0;i<=n;i++)
+	{
+	int j=i*i*i;
+	System.out.println("cube of number:"+j);
+	sum=j+sum;
+	}
+	System.out.println(sum);
+	}
+}	
