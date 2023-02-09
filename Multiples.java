52e5b93964d6a7c0c65cadfac9838eb34a4f4b91
S.Sridhar <124337210+iamsridha@users.noreply.github.com>
2023-02-09 16:08:05 +0530

    Add files via upload

---
+++ Multiples.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Multiples
+{
+public static void main(String[] args)
+{
+int i=1000,sum=0;
+for(i=0;i<=1000;i++)
+   {
+   if(i%3==0||i%5==0)
+	
+   sum=i+sum;}
+   System.out.println("sum is="+sum);
+	
+   
+}
+}
