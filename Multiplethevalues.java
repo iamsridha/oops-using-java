b3987e16e26e65a3c2e37491fa4de8773a177318
S.Sridhar <124337210+iamsridha@users.noreply.github.com>
2023-02-09 22:46:03 +0530

    Add files via upload

---
+++ Multiplethevalues.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Multiplethevalues
+{
+public static void main(String[] args)
+{
+int Multiple=0;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the number");
+int a=s.nextInt();
+System.out.println("Enter the number");
+int b=s.nextInt();
+Multiple=a*b;
+System.out.println("Multiple values is="+Multiple);
+}
+}
---
+++ Numberequal.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Numberequal
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the number");
+int a=s.nextInt();
+System.out.println("Enter the number");
+int b=s.nextInt();
+if(a==b)
+{
+System.out.println("True");
+}
+else 
+{
+System.out.println("False");
+}
+}
+}
